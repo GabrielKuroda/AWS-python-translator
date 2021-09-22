b7104919cf0db75a7280eb5706681c36e02c424c
Gabriel Kuroda <gabrielkuroda20@gmail.com>
2021-09-22 18:43:29 -0300

    WebServer | Idioma Origem add

--- ProjetoWebServer/webserver.py
+++ ProjetoWebServer/webserver.py
@@ -13,10 +13,11 @@ import json
 class Requests(BaseHTTPRequestHandler):
 
     urlImage = ""
+    languageOrigin = ""
     languageTarget = ""
     accessKey = ""
     secretAccessKey = ""
-
+    
     def callAWSTranslate(self,phrase):
 
         global languageTarget
@@ -26,24 +27,33 @@ class Requests(BaseHTTPRequestHandler):
         client = boto3.client('translate', region_name="us-east-1",aws_access_key_id= accessKey,
             aws_secret_access_key= secretAccessKey)
 
-        return client.translate_text(Text=phrase, SourceLanguageCode="auto",TargetLanguageCode=languageTarget)
+        return client.translate_text(Text=phrase, SourceLanguageCode=languageOrigin,TargetLanguageCode=languageTarget)
 
     def translate(self,phrase):
-
         return self.callAWSTranslate(phrase)['TranslatedText']
 
     def getLanguageName(self,languageCode):
+        global languageOrigin
 
-        if languageCode in "ja":
-            return "Japanese"
-        elif languageCode in "pt":
-            return "Portuguese"
+        languages = ['Portuguese','Japanese','English','Spanish']
+        if languageOrigin in "pt":
+            languages = ['Português','Japonês','Inglês','Espanhol']
+        elif languageOrigin in "ja":
+            languages = ['ポルトガル語','日本語','英語','スペイン語']
+        elif languageOrigin in "en":
+            languages = ['Portuguese','Japanese','English','Spanish']
+        
+        if languageCode in "pt":
+            return languages[0]
+        elif languageCode in "ja":
+            return languages[1]
         elif languageCode in "en":
-            return "English"
+            return languages[2]
         elif languageCode in "sp":
-            return "Spanish"
+            return languages[3]
         else:
             return "Unknown"
+            
 
     def getLanguageFrom(self,phrase):
 
@@ -59,10 +69,21 @@ class Requests(BaseHTTPRequestHandler):
     def do_GET(self):
         global urlImage
         global languageTarget
+        global languageOrigin
 
-        self._set_response() 
+        self._set_response()
 
-        class_names = ['borboleta', 'cachorro', 'dente-de-leao', 'gafanhoto', 'gato', 'girassol', 'joaninha', 'libelulas', 'margarida', 'monitor', 'mosquito', 'mouse', 'rosa', 'teclado', 'tulipa']
+        class_namesPT = ['borboleta', 'cachorro', 'dente-de-leao', 'gafanhoto', 'gato', 'girassol', 'joaninha', 'libelulas', 'margarida', 'monitor', 'mosquito', 'mouse', 'rosa', 'teclado', 'tulipa']
+        class_namesJA = ['蝶', '犬', 'タンポポ', 'イナゴ', '猫', 'ひまわり', 'テントウムシ', 'トンボ', 'デイジー', 'モニター', '蚊', 'マウス', 'バラ', 'キーボード', 'チューリップ']
+        class_namesEN = ['butterfly', 'dog', 'dandelion', 'locust', 'cat', 'sunflower', 'ladybird', 'dragonflies', 'daisy', 'monitor', 'mosquito', 'mouse', 'rose', 'keyboard', 'tulip']
+
+        if languageOrigin in "pt":
+            class_names = class_namesPT
+        elif languageOrigin in "ja":
+            class_names = class_namesJA
+        elif languageOrigin in "en":
+            class_names = class_namesEN
+
         model = keras.models.load_model('projetoUsandoTransferLearning.pth')
         img_height = 130
         img_width = 130
@@ -77,19 +98,51 @@ class Requests(BaseHTTPRequestHandler):
 
         tranlated = self.translate(result)
 
-        self.wfile.write(bytes("<html><head><title>IA Projeto 6</title></head><meta charset=\"utf-8\" />", "utf-8"))
-        self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<p>Source Language: %s</p>" % self.getLanguageFrom(result), "utf-8"))
-        self.wfile.write(bytes("<p>Result: %s</p>" % result, "utf-8"))
-        self.wfile.write(bytes("<p>Target Language: %s</p>" % self.getLanguageName(languageTarget), "utf-8"))
-        self.wfile.write(bytes("<p>Result Translated: %s</p>" % tranlated, "utf-8"))
-        self.wfile.write(bytes("<br>{:.2f}%<br>".format(100 * np.max(score)), "utf-8"))
-        self.wfile.write(bytes("<img src=\"%s\" alt=\"Image\" width=\"500\" height=\"600\">" % urlImage, "utf-8"))
-        self.wfile.write(bytes("</body></html>", "utf-8"))
+        if languageOrigin in "pt":
+            self.wfile.write(bytes("<html><head><title>Tradutor</title></head><meta charset=\"utf-8\" />", "utf-8"))
+            self.wfile.write(bytes("<body>", "utf-8"))
+            self.wfile.write(bytes("<p>Idioma Origem: %s</p>" % self.getLanguageFrom(result), "utf-8"))
+            self.wfile.write(bytes("<p>Resultado: %s</p>" % result, "utf-8"))
+            self.wfile.write(bytes("<p>Idioma alvo: %s</p>" % self.getLanguageName(languageTarget), "utf-8"))
+            self.wfile.write(bytes("<p>Resultado Tradução: %s</p>" % tranlated, "utf-8"))
+            self.wfile.write(bytes("<br>{:.2f}%<br>".format(100 * np.max(score)), "utf-8"))
+            self.wfile.write(bytes("<img src=\"%s\" alt=\"Image\" width=\"500\" height=\"600\">" % urlImage, "utf-8"))
+            self.wfile.write(bytes("</body></html>", "utf-8"))
+        elif languageOrigin in "ja":
+            self.wfile.write(bytes("<html><head><title>翻訳</title></head><meta charset=\"utf-8\" />", "utf-8"))
+            self.wfile.write(bytes("<body>", "utf-8"))
+            self.wfile.write(bytes("<p>ソース言語: %s</p>" % self.getLanguageFrom(result), "utf-8"))
+            self.wfile.write(bytes("<p>結果: %s</p>" % result, "utf-8"))
+            self.wfile.write(bytes("<p>目標言語: %s</p>" % self.getLanguageName(languageTarget), "utf-8"))
+            self.wfile.write(bytes("<p>翻訳結果: %s</p>" % tranlated, "utf-8"))
+            self.wfile.write(bytes("<br>{:.2f}%<br>".format(100 * np.max(score)), "utf-8"))
+            self.wfile.write(bytes("<img src=\"%s\" alt=\"Image\" width=\"500\" height=\"600\">" % urlImage, "utf-8"))
+            self.wfile.write(bytes("</body></html>", "utf-8"))
+        elif languageOrigin in "en":
+            self.wfile.write(bytes("<html><head><title>Translator</title></head><meta charset=\"utf-8\" />", "utf-8"))
+            self.wfile.write(bytes("<body>", "utf-8"))
+            self.wfile.write(bytes("<p>Origin Language: %s</p>" % self.getLanguageFrom(result), "utf-8"))
+            self.wfile.write(bytes("<p>Result: %s</p>" % result, "utf-8"))
+            self.wfile.write(bytes("<p>Target Language: %s</p>" % self.getLanguageName(languageTarget), "utf-8"))
+            self.wfile.write(bytes("<p>Result Translated: %s</p>" % tranlated, "utf-8"))
+            self.wfile.write(bytes("<br>{:.2f}%<br>".format(100 * np.max(score)), "utf-8"))
+            self.wfile.write(bytes("<img src=\"%s\" alt=\"Image\" width=\"500\" height=\"600\">" % urlImage, "utf-8"))
+            self.wfile.write(bytes("</body></html>", "utf-8"))
+        else:
+            self.wfile.write(bytes("<html><head><title>Translator</title></head><meta charset=\"utf-8\" />", "utf-8"))
+            self.wfile.write(bytes("<body>", "utf-8"))
+            self.wfile.write(bytes("<p>Origin Language: %s</p>" % self.getLanguageFrom(result), "utf-8"))
+            self.wfile.write(bytes("<p>Result: %s</p>" % result, "utf-8"))
+            self.wfile.write(bytes("<p>Target Language: %s</p>" % self.getLanguageName(languageTarget), "utf-8"))
+            self.wfile.write(bytes("<p>Result Translated: %s</p>" % tranlated, "utf-8"))
+            self.wfile.write(bytes("<br>{:.2f}%<br>".format(100 * np.max(score)), "utf-8"))
+            self.wfile.write(bytes("<img src=\"%s\" alt=\"Image\" width=\"500\" height=\"600\">" % urlImage, "utf-8"))
+            self.wfile.write(bytes("</body></html>", "utf-8"))
 
     def do_POST(self):
         global urlImage
         global languageTarget
+        global languageOrigin
         global accessKey
         global secretAccessKey
 
@@ -99,6 +152,7 @@ class Requests(BaseHTTPRequestHandler):
         resultPost = json.loads(post_data.decode('utf-8'))
         urlImage = resultPost['image']
         languageTarget = resultPost['languageTarget']
+        languageOrigin = resultPost['languageOrigin']
         accessKey = resultPost['accessKey']
         secretAccessKey = resultPost['secretAccessKey']
         
